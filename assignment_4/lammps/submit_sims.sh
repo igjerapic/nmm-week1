d8d4214fc594a5af9ab0ba39cc4ad13f5b2380a4
igjerapic24 <igjerapic@gmail.com>
2025-04-22 03:15:26 +0200

    finalized simulation submission script

--- assignment_4/lammps/submit_sims.sh
+++ assignment_4/lammps/submit_sims.sh
@@ -1,19 +1,52 @@
 #!/bin/bash
 
-# submit each of the simulations for fixed packing fraction
-eta=0.45
-for temperature in 1.0 0.9 0.8 0.7; do
-    dir_name="temp${temperature}_eta${eta}"
-    echo "submitting simulation in $dir_name"
-
-    (cd "$dir_name" && sbatch submit.sh)
-done
+eta=0.5
 temperature=1.0
-for eta in 0.7 0.6 0.5 0.4; do
-    dir_name="temp${temperature}_eta${eta}"
-    echo "submitting simulation in $dir_name"
+num_part=4096 # number of particles
 
-    (cd "$dir_name" && sbatch submit.sh)
+# for cut-off ranges
+# LJ cutoff
+ntasks=1
+coul_cut=0.0
+for lj_cut in 1.12 2.0 4.0 6.0; do
+    dir_name="temp${temperature}_eta${eta}_LJ${lj_cut}_COUL${coul_cut}_N${num_part}_M${ntasks}"
+    # echo "submitting simulation in $dir_name"
+    cd "$dir_name"
+    # (cd "$dir_name" && sbatch submit.sh)
+done
+
+# Coulomb cutoff
+ntasks=1     
+lj_cut=1.12
+for coul_cut in 1.12 2.0 4.0 6.0; do
+    dir_name="temp${temperature}_eta${eta}_LJ${lj_cut}_COUL${coul_cut}_N${num_part}_M${ntasks}" 
+    # echo "submitting simulation in $dir_name"
+    cd "$dir_name"
+    # (cd "$dir_name" && sbatch submit.sh)
+done
+
+# for range of particle numbers
+ntasks=1  
+for coul_cut in 0.0 6.0; do
+    for mult in 2 4 8 16; do
+        N=$(( mult *  num_part ))
+        dir_name="temp${temperature}_eta${eta}_LJ${lj_cut}_COUL${coul_cut}_N${N}_M${ntasks}"
+        # echo "submitting simulation in $dir_name"
+        cd "$dir_name"
+        # (cd "$dir_name" && sbatch submit.sh)
+    done
+done
+
+# dfferent num of processers used
+lj_cut=1.12
+for coul_cut in 0.0 6.0; do
+    for ntasks in 2 4 8 16 32; do
+        N=$(( num_part * 2 ))
+        dir_name="temp${temperature}_eta${eta}_LJ${lj_cut}_COUL${coul_cut}_N${N}_M${ntasks}"
+        # echo "submitting simulation in $dir_name"
+        cd "$dir_name"
+       # (cd "$dir_name" && sbatch submit.sh)
+    done
 done
 
 
